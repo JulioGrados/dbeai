db8cec57df9d4d818850958d00515f9558dda752
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2026-03-14 15:55:07 -0500

    [14/3/25] - blog

---
+++ models/blog.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const timestamps = require('mongoose-timestamp')
+
+const BlogSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    title: {
+      type: String
+    },
+    subject: {
+      type: String
+    },
+    content: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    collection: 'blogs'
+  }
+)
+
+BlogSchema.plugin(timestamps, {
+  createdAt: { index: true },
+  updatedAt: { index: true }
+})
+
+BlogSchema.index({
+  linked: 1,
+  title: 1
+})
+
+module.exports = mongoose.model('Blog', BlogSchema)
